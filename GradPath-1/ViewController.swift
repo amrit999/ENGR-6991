55ad1b06b393b2e403dc8aa7d6c75dd3da2f6f8d
amrit kaur <amritkalsi1996@gmail.com>
2020-10-18 21:34:06 -0400

    test setup

---
+++ GradPath-1/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GradPath-1
+//
+//  Created by Amrit Kalsi on 2020-10-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func LoginTapped(_ sender: Any) {
+        
+        print("login tapped")
+    }
+    
+    @IBAction func SignupTapped(_ sender: Any) {
+        print("signup tapped")
+    }
+    
+    
+}
+
